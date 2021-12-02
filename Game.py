cdf6a58f3194483492dd7fcd2752bf22629e5986
GiorgosGiannoglou <89971174+GiorgosGiannoglou@users.noreply.github.com>
2021-12-02 11:01:01 +0100

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,45 @@
+
+print("███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████")
+print("█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░███░░░░░░░░░░█░░░░░░░░░░░░░░███░░░░░░░░░░░░░░████░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█")
+print("█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█")
+print("█░░░░░░▄▀░░░░░░█░░▄▀░░░░░░░░▄▀░░███░░░░▄▀░░░░█░░▄▀░░░░░░▄▀░░███░░▄▀░░░░░░░░░░████░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█")
+print("█████░░▄▀░░█████░░▄▀░░████░░▄▀░░█████░░▄▀░░███░░▄▀░░██░░▄▀░░███░░▄▀░░████████████░░▄▀░░█████████░░▄▀░░█████████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░█████████")
+print("█████░░▄▀░░█████░░▄▀░░░░░░░░▄▀░░█████░░▄▀░░███░░▄▀░░░░░░▄▀░░░░█░░▄▀░░░░░░░░░░████░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░░░█░░▄▀░░█████████░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█")
+print("█████░░▄▀░░█████░░▄▀▄▀▄▀▄▀▄▀▄▀░░█████░░▄▀░░███░░▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░█████████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█")
+print("█████░░▄▀░░█████░░▄▀░░░░░░▄▀░░░░█████░░▄▀░░███░░▄▀░░░░░░░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░░░░░░░░░█░░░░░░░░░░▄▀░░█░░▄▀░░█████████░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░░░█")
+print("█████░░▄▀░░█████░░▄▀░░██░░▄▀░░███████░░▄▀░░███░░▄▀░░████░░▄▀░░█░░▄▀░░████████████░░▄▀░░█████████████████░░▄▀░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░█████████░░▄▀░░█████████")
+print("█████░░▄▀░░█████░░▄▀░░██░░▄▀░░░░░░█░░░░▄▀░░░░█░░▄▀░░░░░░░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░░░░░░░░░█░░░░░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░█░░▄▀░░█████████░░▄▀░░░░░░░░░░█")
+print("█████░░▄▀░░█████░░▄▀░░██░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░█████████░░▄▀▄▀▄▀▄▀▄▀░░█")
+print("█████░░░░░░█████░░░░░░██░░░░░░░░░░█░░░░░░░░░░█░░░░░░░░░░░░░░░░█░░░░░░░░░░░░░░████░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░█████████░░░░░░░░░░░░░░█")
+print("███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████")
+
+print("")
+print("(Je moet de hele antwoord uittypen!)")
+print("")
+print("Je word achterna gezeten door de Zulu. ESCAPE!!!")   
+print("")
+vraag1 = input ("Je vindt 2 padden. Ga je links of rechts? ").lower()
+if vraag1 == "links" :
+    print("Het is een klif, je valt tot je dood.")
+else:
+    vraag2 = input ("Je ziet een beer, een pistool en Obama, waar ga je? ").lower()
+    if vraag2 == "beer" :
+        print("Je vindt een beer. Je springt op zijn rug, en jullie rennen samen weg")
+        vraag3 = input("Jullie komen tegen een leeuw en honing, waar gaan jullie ").lower()
+        if vraag3 == "leeuw" :
+            vraag4 = input("De beer komt in een gevecht tegen de leeuw, wint de beer of de leeuw? ").lower()
+            if vraag4 == "beer" :
+                print("De leeuw is nu dood, maar na de gevecht is de beer honger. Het eet je op!")
+            else:
+                print("De beer is dood en de leeuw eet je op")
+        else:
+            print("De beer eet de honing op. Een bijenzwerm word boos en eet jullie allebei op!")
+    elif vraag2 == ('pistool'):
+     print("Je probeert de tribe te schieten, maar de pistool is defectief en het ontploft. Je gaat dood!")
+    else:
+        print("Obama heeft een helikopter. Jullie vliegen samen weg, maar de tribe heeft ook een heli en volgt jullie .")
+        vraag5 = input("Je kijkt rond de heli, je vind een granaat en kaas, wat gebruik je? ").lower()
+        if vraag5 == "granaat" :
+            print("Je gooit de granaat naar de tribe's heli, het stuitert terug naar je heli. Je gaat dood!")
+        else:
+            print("Je gooit de kaas naar de grond, de tribe is honger en volgt het, ze hebben een slechte landing en gaan allemaal dood. YOU ESCAPE!!!!!")
